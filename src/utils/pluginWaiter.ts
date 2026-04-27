39611c21c78b79a83f6135af785c66e403faab93
甚吊圣人 <1321075268@qq.com>
2026-04-27 22:14:59 +0800

    feat: 插件等待工具添加超时处理

--- src/utils/pluginWaiter.ts
+++ src/utils/pluginWaiter.ts
@@ -1,21 +1,33 @@
 import mineflayer from 'mineflayer';
 
-function waitPluginLoad(bot: mineflayer.Bot, pluginName: string) {
-  return new Promise(resolve => {
+function waitPluginLoad(bot: mineflayer.Bot, pluginName: string, timeOut: number = 10000) {
+  return new Promise((resolve, reject) => {
+    let timer: NodeJS.Timeout | null = null;
+    if (timeOut >= 0) {
+      timer = setTimeout(() => {
+        reject(new Error(`Plugin ${pluginName} not loaded within ${timeOut}ms`));
+      }, timeOut);
+    }
+
     if (bot.pluginLoadMap?.[pluginName]) {
+      timer && clearTimeout(timer);
       resolve(1);
     } else {
       // @ts-ignore
-      bot.once(`pluginLoaded_${pluginName}`, resolve);
+      bot.once(`pluginLoaded_${pluginName}`, () => {
+        timer && clearTimeout(timer);
+        resolve(1);
+      });
     }
+    
   })
 }
 
-function waitPluginLoads(bot: mineflayer.Bot, pluginName: string | string[]) {
+function waitPluginLoads(bot: mineflayer.Bot, pluginName: string | string[], timeOut: number = 10000) {
   if (Array.isArray(pluginName)) {
-    return Promise.all(pluginName.map(name => waitPluginLoad(bot, name)));
+    return Promise.all(pluginName.map(name => waitPluginLoad(bot, name, timeOut)));
   }
-  return waitPluginLoad(bot, pluginName);
+  return waitPluginLoad(bot, pluginName, timeOut);
 }
 
 function pluginReady(bot: mineflayer.Bot, pluginName: string) {
